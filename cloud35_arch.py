f7c5ffb8c7f9a980464eaed2aadbea445b9a4fbe
Kamesh Akella <kamesh.asp@gmail.com>
2024-03-27 20:21:41 -0400

    feedback implemented
    Signed-off-by: Kamesh Akella <kamesh.asp@gmail.com>

--- cloud35_arch.py
+++ cloud35_arch.py
@@ -12,35 +12,28 @@ graph_attr = {
 
 with Diagram("Cloud35 Scale Lab Architechture", show=False, graph_attr=graph_attr):
     external_user = [Users("External User"),]
-    internet_gateway = Router("Internet Gateway")
+    internet_gateway = Router("Public Network (lab dhcp)")
     external_user >> internet_gateway
-
-    with Cluster("OpenShift Cluster"):
-        with Cluster("Control Plane Nodes"):
-            control_plane_nodes = [Server("Control Plane Node 1"),
-                                   Server("Control Plane Node 2"),
-                                   Server("Control Plane Node 3")]
-
-        with Cluster("Worker Plane Nodes"):
-            worker_nodes = [Server("Worker Node 1"),
-                            Server("Worker Node 2"),
-                            Server("Worker Node 3")]
-        
-        # Representing the Ingress Load Balancer
-        ingress_lb = Nginx("Ingress Load Balancer")
-        internet_gateway >> ingress_lb
-
-        ingress_lb >> worker_nodes
-
-    # External DB positioned outside the cluster for clarity
-    db = PostgreSQL("External DB (PostgreSQL)")
-
-    with Cluster("Load Generators"):
-        load_gens = [Server("Load Gen 1"),
-                     Server("Load Gen 2"),
-                     Server("Load Gen 3")]
-    load_gens >> internet_gateway
-
-    # Connection from the OpenShift Cluster to the External DB
-    for wn in worker_nodes:
-        wn >> db
+    with Cluster("Private Network (Scale Lab Network 1)"):
+        with Cluster("OpenShift Cluster"):
+            with Cluster("Control Plane Nodes", direction="LR"):
+                cnode1 = Server("Control Plane Node 1")
+                cnode2 = Server("Control Plane Node 2")
+                cnode3 = Server("Control Plane Node 3")
+            cnode1 - cnode2 - cnode3
+            with Cluster("Worker Plane Nodes"):
+                worker_nodes = [Server("Worker Node 3"),
+                                Server("Worker Node 2"),
+                                Server("Worker Node 1")]
+                ingress_lb = Nginx("Ingress Load Balancer")
+            # Representing the Ingress Load Balancer
+            internet_gateway >> ingress_lb
+            ingress_lb >> worker_nodes
+            db = PostgreSQL("External DB (PostgreSQL)")
+            for wn in worker_nodes:
+                wn >> db
+        with Cluster("Load Generators"):
+                load_gens = [Server("Load Gen 3"),
+                            Server("Load Gen 2"),
+                            Server("Load Gen 1")]
+        load_gens >> ingress_lb
